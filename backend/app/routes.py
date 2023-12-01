267199a42fb7e0ad5d62d1753dfa316fcd523e60
Yashwanth Reddy Veesarapu <yashwanthreddyveesarapu@gmail.com>
2023-12-01 13:11:22 -0500

    Fixed venv issues and Users backend route

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,5 @@
 from app import app, mongo
-from flask import render_template, request, jsonify
-
+from flask import render_template, request, jsonify, make_response
 
 
 @app.route("/", methods=["GET"])
@@ -18,10 +17,21 @@ def fetch_gmail_data():
 @app.route("/users", methods=["POST", "GET"])
 def user_data():
     if request.method == "POST":
-    #    email = body.email
-    #    displayName = body.displayName
-    #    uid = body.uid
-    #    token = body.token
+        body = request.get_json()
+        required_params = ["email", "displayName", "uid", "token"]
+        if not all(param in body for param in required_params):
+            return make_response("Bad parameters. Required parameters are: email, displayName, uid, token", 400)
+        users_collection = mongo.db.users
 
-    #    user = mongo.db.users.findOne({email: email})
-       return request.method
+        # Check if the user already exists using the email
+        existing_user = users_collection.find_one({"email": body["email"]})
+        if existing_user:
+            return make_response("User with this email already exists", 400)
+        
+        # Insert the new user into the 'users' collection
+        users_collection.insert_one(body)
+
+        return make_response("User added successfully", 200)  # 201 indicates resource creation success
+    if request.method == "GET":
+        return make_response("GET request handled", 200)
+
