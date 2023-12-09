4c0815c3c71e3966b0b9118dfa31f56324d5d996
Yashwanth Reddy Veesarapu <yashwanthreddyveesarapu@gmail.com>
2023-12-09 01:19:04 -0500

    Minor changes

--- out/_next/static/chunks/app/chat/page-61567d8dfb0f03a2.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[929],{7194:function(e,r,t){Promise.resolve().then(t.bind(t,2436))},2436:function(e,r,t){"use strict";t.r(r);var n=t(7437),o=t(4585),s=t(2265);r.default=()=>{let[e,r]=(0,s.useState)(""),[t,i]=(0,s.useState)([]);return(0,n.jsxs)(o.Z,{children:[(0,n.jsx)("h2",{children:"Google Integrations"}),(0,n.jsxs)("p",{children:["We are working on Google Calendar and TODO list implemention.... ",(0,n.jsx)("br",{}),"Stay tuned for updates"]})]})}},4585:function(e,r,t){"use strict";var n=t(7437);t(5263),t(2265),r.Z=e=>{let{children:r}=e;return(0,n.jsx)("div",{className:"container",children:r})}},5263:function(){},622:function(e,r,t){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var n=t(2265),o=Symbol.for("react.element"),s=Symbol.for("react.fragment"),i=Object.prototype.hasOwnProperty,u=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,a={key:!0,ref:!0,__self:!0,__source:!0};function q(e,r,t){var n,s={},c=null,l=null;for(n in void 0!==t&&(c=""+t),void 0!==r.key&&(c=""+r.key),void 0!==r.ref&&(l=r.ref),r)i.call(r,n)&&!a.hasOwnProperty(n)&&(s[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===s[n]&&(s[n]=r[n]);return{$$typeof:o,type:e,key:c,ref:l,props:s,_owner:u.current}}r.Fragment=s,r.jsx=q,r.jsxs=q},7437:function(e,r,t){"use strict";e.exports=t(622)}},function(e){e.O(0,[971,864,744],function(){return e(e.s=7194)}),_N_E=e.O()}]);
